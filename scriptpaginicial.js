8b6ceec4cab471d2f839c1835573b6ea11e96135
marcosv00 <marcosvbdc970@gmail.com>
2023-10-09 22:04:17 -0300

    adicionei um botão de voltar ao topo e coloquei links em tags que falatva e troquei algumas tags por questão de semantica no projeto

--- scriptpaginicial.js
+++ scriptpaginicial.js
@@ -11,4 +11,23 @@ function myFunction() {
   }
 }
 
-  
+
+const btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
+
+document.addEventListener('scroll',ocultar)
+
+function ocultar(){
+    if(window.scrollY > 1600){
+      btn.style.display = "flex"
+      
+    } else {
+      btn.style.display = "none"
+    }
+
+}
+
+ocultar()
+
