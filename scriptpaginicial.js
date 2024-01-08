0ba7fddfd9cdf8099e1155193b0bf0778ce1f406
marcosv00 <marcosvbdc970@gmail.com>
2024-01-08 11:14:32 -0300

    deixando o código legivel

--- scriptpaginicial.js
+++ scriptpaginicial.js
@@ -1,28 +1,28 @@
-window.onscroll = function(){myFunction()};
+window.onscroll = function(){navbarScroll()};
 
 var navbar = document.getElementById("menu");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function navbarScroll() {
   if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky");
+    navbar.classList.add("sticky"); // adicione uma propriedade css chamada stick assim que a barra é movida para baixo, fazendo com que o menu vá junto
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky");// vai remover a propriedade stick se rolar para cima a barra horizontal. 
   }
 }
 
 
-const btn = document.querySelector("#back-to-top");
-btn.addEventListener("click", function() {
+const btn = document.querySelector("#back-to-top");// eu usei dom e selecionei o id do elemento que representar a imagem do botão "voltar ao topo"
+btn.addEventListener("click", function() { // adicionei um evento de que quando clicar no elemento é para ele voltar
     window.scrollTo(0, 0);
 });
 
 document.addEventListener('scroll',ocultar)
-document.addEventListener(anima)
-document.addEventListener(desanima)
+document.addEventListener(aparece)
+document.addEventListener(desaparece)
 
 
-function anima(){
+function aparece(){
   if(window.scrollY > 1450){
     btn.style.transition = "linear 0.3s"
     btn.style.padding = "25px"
@@ -37,7 +37,7 @@ function anima(){
 
 
 
-function desanima(){
+function desaparece(){
   if(window.scrollY < 1450){
     btn.style.transition = "linear 0.3s"
     btn.style.bottom = "0px"
@@ -53,16 +53,16 @@ function ocultar(){
 
     if(window.scrollY > 1450){
       btn.style.display = "flex";
-      anima()
+      aparece()
     } else {
-      desanima()
+      desaparece()
     }
     
 
 }
 
-
-function myFuncti(){
+// quando clicar na navabar para mobile ele vai mostrar o menu
+function tela_celular(){
   var navbar = document.getElementById('menu');
   navbar.classList.toggle('show');
 }
